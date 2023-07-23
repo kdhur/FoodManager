168baed237ecb1752962b801ad8a99e02bff778c
Kyeongdoo Hur <1kdhur2@gmail.com>
2023-07-23 22:25:21 +0900

    init commit

---
+++ src/test/java/com/fm/foodmanager/FoodManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fm.foodmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
